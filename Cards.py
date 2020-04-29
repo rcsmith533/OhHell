9d7332a60dca7e91bbe8739fa301c3c3958d0584
Connor Smith <titdirt994@gmail.com>
2020-04-29 13:14:46 -0500

    Add files via upload

--- Cards.py
+++ Cards.py
@@ -1,5 +1,8 @@
-deck = []
 import random
+import math
+deck = []
+players = {}
+gameRound = 0
 
 def genDeck(deck):
         nums = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
@@ -11,8 +14,29 @@ def genDeck(deck):
             s += 1
         return deck
 
-d = genDeck(deck)
+def addPlayer(playerName):
+    players[playerName] = {'Score':0,'Bids':[],'Hand':[]}
 
-print(d)
+def dealCards(shuffeledDeck):
+    cardsDealt = 0
+    while cardsDealt < math.floor(52/len(players)):
+        for i in players:
+            players[i]['Hand'].append(shuffeledDeck.pop())
+        cardsDealt += 1
+    
+def legalCards(hand):
+    #TODO
+    #Returns a list of cards that can be played
+    return None
+        
+d = genDeck(deck)
 random.shuffle(d)
-print(d)
+addPlayer('Connor')
+addPlayer('Sarah')
+addPlayer('Joe')
+addPlayer('Al')
+addPlayer('Luke')
+addPlayer('Alesha')
+dealCards(d)
+for i in players:
+    print(i, players[i]['Hand'],len(players[i]['Hand']))
