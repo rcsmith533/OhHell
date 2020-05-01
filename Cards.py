a838083ccecd678ced969fd9972166c27844710c
Connor Smith <titdirt994@gmail.com>
2020-04-30 21:50:34 -0500

    Add files via upload

--- Cards.py
+++ Cards.py
@@ -1,11 +1,10 @@
 import random
 import math
-deck = []
-players = {}
-gameRound = 0
-dealer = 0
+from collections import OrderedDict
+players = OrderedDict()
 
-def genDeck(deck):
+def genDeck():
+        deck = []
         nums = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
         suits = ['S','H','C','D']
         s = 0
@@ -40,27 +39,9 @@ def legalCards(hand):
     #TODO
     #Returns a list of cards that can be played
     return None
-        
-d = genDeck(deck)
-random.shuffle(d)
-addPlayer('Connor')
-addPlayer('Sarah')
-addPlayer('Joe')
-addPlayer('Al')
-#addPlayer('Luke')
-#addPlayer('Alesha')
-#addPlayer('Brad')
-#addPlayer('Rachel')
-#addPlayer('Cammie')
-#addPlayer('Brandon')
-#addPlayer('Hannah')
-#addPlayer('Hailey')
-dealCards(d)
-for i in players:
-    print(i, players[i]['Hand'],len(players[i]['Hand']))
 
-def startRound(deck,numOfCards):
-    d = genDeck(deck)
+def startRound(numOfCards):
+    d = genDeck()
     random.shuffle(d)
     dealCards(d)
 
@@ -76,37 +57,61 @@ def playersHaveCards():
     else:
         return True
 
-d = genDeck(deck)
+def genRounds():
+    maxRounds = ((math.floor(52/len(players)))*2)-1
+    x = 0
+    cards = math.floor(52/len(players))
+    roundCardCount = []
+    halfwayPoint = math.ceil(maxRounds/2)
+    while x < maxRounds:
+        if x < halfwayPoint-1:
+            roundCardCount.append(cards)
+            cards -= 1
+            x += 1
+        else:
+            roundCardCount.append(cards)
+            cards += 1
+            x += 1
+    return roundCardCount
+
+def printHands():
+    for i in players:
+        print(i, players[i]['Hand'],len(players[i]['Hand']))
+
+def swapDealer():
+    players.move_to_end(list(players.keys())[0])
+
+d = genDeck()
 random.shuffle(d)
 clearHands()
-dealCardsRound(d,1)
-for i in players:
-    print(i, players[i]['Hand'],len(players[i]['Hand']))
+
+addPlayer('Connor')
+addPlayer('Sarah')
+addPlayer('Joe')
+addPlayer('Al')
+addPlayer('p1')
+addPlayer('p2')
+addPlayer('p3')
+addPlayer('p4')
 
 def startGame():
-    maxRounds = (math.floor(52/len(players)))*2
-    gameRound = 1
-    while gameRound <= maxRounds:
-        print('Round:',gameRound)
-        print('Total Cards:',totalCardsDealt())
-        if gameRound == 1:
-            startRound(deck,maxRounds/2)
-            while playersHaveCards():
-                for i in players:
-                    c = players[i]['Hand'].pop()
-                    print(i,c)
-        else:
-            startRound(deck,(maxRounds/2)-(gameRound-1))
-            while playersHaveCards():
-                for i in players:
-                    c = players[i]['Hand'].pop()
-                    print(i,c)
+    maxRounds = ((math.floor(52/len(players)))*2)-1
+    gameRound = 0
+    cardCount = genRounds()
+    while gameRound < maxRounds:
+        print('Round:',gameRound + 1)
+        print('Total Cards:',cardCount[gameRound])
+        startRound(cardCount[gameRound])
+        while playersHaveCards():
+            for i in players:
+                c = players[i]['Hand'].pop()
+                print(i,c)
+        swapDealer()
         gameRound += 1
 
 clearHands()
 startGame()
-for i in players:
-    print(i, players[i]['Hand'],len(players[i]['Hand']))
+
 
 
     
